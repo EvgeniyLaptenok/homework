ab43ab9af658bcf0b7ac0d07c76c78d79eb46859
evgeniy <shtorm20300@gmail.com>
2022-11-30 22:11:29 +0600

    задача_3

---
+++ z3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+string num1 = Console.ReadLine()!;
+int inputNum1 = int.Parse(num1);
+
+int div = inputNum1 % 2;
+
+if (div == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
