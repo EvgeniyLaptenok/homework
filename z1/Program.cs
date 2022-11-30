6634b752f9445442085d5d23ebc1f7990b9615e4
evgeniy <shtorm20300@gmail.com>
2022-11-30 21:24:02 +0600

    задача_1

---
+++ z1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число: ");
+string num1 = Console.ReadLine()!;
+int inputNum1 = int.Parse(num1);
+
+Console.Write("Введите второе число: ");
+string num2 = Console.ReadLine()!;
+int inputNum2 = int.Parse(num2);
+
+int max = inputNum1;
+
+if (max < inputNum2)
+{
+    Console.WriteLine(max = inputNum2);
+}
+else 
+{
+    Console.WriteLine(max);
+}
