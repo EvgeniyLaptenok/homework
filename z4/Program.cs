5b7c4c59cf7e5e4c70a43ba92e527dff2184a051
evgeniy <shtorm20300@gmail.com>
2022-11-30 23:12:41 +0600

    задача_4

---
+++ z4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+string N = Console.ReadLine()!;
+int inputN = int.Parse(N);
+
+int count = 1;
+
+while (count <= inputN)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count++;
+}
